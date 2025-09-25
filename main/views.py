4816182842444f991ad753aa40f1e7df8828ad8b
Phillip Kaiserty <phillipkaiserty2008@gmail.com>
2025-09-25 22:03:58 +0200

    Fix Views.py mistake from housekeeping

--- main/views.py
+++ main/views.py
@@ -1,8 +1,6 @@
 #REQUEST RESPONSE LOGIC
-from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Informatik Projekt 2025!")
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the main index.")
