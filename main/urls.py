f3ee53f4ff6152c8714a5b59f23a4b3e0e929b8b
Leonard Heidrich <leonardcheidrich@gmail.com>
2025-10-15 14:05:14 +0200

    Added login interfaces and made login the main url. Registration works through the login page now and  through the Admin panel

--- main/urls.py
+++ main/urls.py
@@ -4,8 +4,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.login_view, name="login"),  # Root URL now loads login page
     path("vote/", views.vote, name="vote"),
     path("results/", views.results, name="results"),
-    path("login/", views.login, name="login"),
+    path("login/", views.login_view, name="login"),
+    path("register/", views.register_view, name="register"),
 ]
