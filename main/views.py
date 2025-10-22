c84416e4cf77f7bd893d05a5fe62a2fd409eb5b8
Phillip Kaiserty <phillipkaiserty2008@gmail.com>
2025-10-22 18:38:24 +0200

    changed index to home and called it home page

--- main/views.py
+++ main/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, "main/index.html")
+def home(request):
+    return render(request, "main/home.html")
 
 def vote(request):
     return render(request, "main/vote.html")
