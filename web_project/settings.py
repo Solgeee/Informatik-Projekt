7f2e4db9817c632c963a7900bb203010b6addd3f
Leonard Heidrich <leonardcheidrich@gmail.com>
2025-11-27 11:59:41 +0100

    email sending works, still problem with redirecting after registration

--- web_project/settings.py
+++ web_project/settings.py
@@ -152,6 +152,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com" # e.g., 'smtp.sendgrid.net' or 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "projektinformatik5@gmail.com"
-EMAIL_HOST_PASSWORD = "InfProj2025"
+EMAIL_HOST_PASSWORD = "ubkwgynftoazkiyt"
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
